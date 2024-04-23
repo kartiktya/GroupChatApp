f0483fa010229adeec5fcbbe4898e923d41786f2
kartiktya <kartiktyagi953@gmail.com>
2024-04-24 01:03:12 +0530

    added signup page

---
+++ public/signup/signup.js
@@ -0,0 +1,27 @@
+function handleFormSubmit(event) {
+
+    event.preventDefault();
+
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    let userObject = {
+        name: name,
+        email: email,
+        password: password
+    };
+
+    axios.post("http://localhost:3000/user/signup", userObject)
+    .then((response) => {
+        //console.log(response);
+        
+        document.body.innerHTML = document.body.innerHTML + '<h4 style="text-align:center; color:blue;"> Successfully created. You can login now. </h4>'
+        window.location.href = '../login/login.html';
+    })
+    .catch((error)=> {
+        document.body.innerHTML = document.body.innerHTML + '<h4 style="text-align:center; color:red;"> Something went wrong </h4>'
+        console.log(error);
+     });
+}
+
